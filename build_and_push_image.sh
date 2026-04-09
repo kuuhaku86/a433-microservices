65acde718bdf34594e642348bebac80db98b8f43
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2026-04-09 16:36:31 +0000

    Add Dockerfile and script to build + push

---
+++ build_and_push_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 1. Build Docker image
+docker build -t ghcr.io/kuuhaku86/order-service:latest .
+
+# 2. Login to Docker Hub
+echo $GITHUB_PAT | docker login ghcr.io -u kuuhaku86 --password-stdin
+
+# 3. Push image to Docker Hub
+docker push ghcr.io/kuuhaku86/order-service:latest
